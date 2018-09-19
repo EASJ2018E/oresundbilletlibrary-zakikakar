a56fff3fade2d32a2a902a073b280178e51ca2d4
zakikakar <36473212+zakikakar@users.noreply.github.com>
2018-09-19 10:27:43 +0200

    Opgave 9 og 10 tilføjet

---
+++ ØresundsbroenBilletLibrary/ØresundsbroenBilletLibrary/ØresundBil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BilletLibrary;
+
+namespace OeresundsbroenBilletLibrary
+{
+    public class ØresundBil : Bil
+    {
+        public override decimal Pris() //Ved kald af metoden Pris() returneres decimal 410
+        {
+            if (AnvendtBrobizz)
+            {
+                return 161;
+            }
+
+            return 410;
+
+            //if (AnvendtBrobizz) //Tjekker om brobizz er anvendt, hvis true så giver den rabat på 5%
+            //{
+            //    return 240 * Rabat;
+            //}
+
+
+        }
+
+        public override string Køretøj() //Ved kald af metoden Køretøj() returneres string "Øresund Bil"
+        {
+            return "Øresund Bil";
+        }
+    }
+}
---
+++ ØresundsbroenBilletLibrary/ØresundsbroenBilletLibrary/ØresundMC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BilletLibrary;
+
+namespace OeresundsbroenBilletLibrary
+{
+    public class ØresundMC : MC
+    {
+        public override decimal Pris() //Ved kald af metoden Pris() returneres decimal 125
+        {
+            if (AnvendtBrobizz)
+            {
+                return 73;
+            }
+
+            return 210;
+        }
+
+        public override string Køretøj()//Ved kald af metoden Køretøj() returneres string "MC"
+        {
+            return "Øresund MC";
+        }
+    }
+}
